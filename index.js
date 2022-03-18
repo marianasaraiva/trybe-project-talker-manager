5b3140d671599cb78c026e6c75adc8d168cb34ac
Mariana Saraiva <marianascm8@gmail.com>
2022-03-18 18:31:23 -0300

    feat: Requirement 02

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ const talkerResult = await getTalker();
 res.status(HTTP_OK_STATUS).json(talkerResult);
 });
 
+// Requisito 02
+app.get('/talker/:id', async (req, res, _next) => {
+  const { id } = req.params;
+  console.log(id);
+  const talkerResult = await getTalker();
+  console.log(talkerResult);
+  const findTalkerById = talkerResult.find((talker) => talker.id === +id);
+  if (!findTalkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(findTalkerById);
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
