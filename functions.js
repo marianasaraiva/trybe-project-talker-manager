a6ed1dfa272da5eb980d3238400646b3248ae7d8
Mariana Saraiva <marianascm8@gmail.com>
2022-03-22 18:46:50 -0300

    fix: update conditional password
    Co-authored-by: Tainá Santana <tainnaps@gmail.com>

--- functions.js
+++ functions.js
@@ -37,7 +37,7 @@ function validPassword(req, res, next) {
   if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
-  if (password.length < 5) {
+  if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   next();
