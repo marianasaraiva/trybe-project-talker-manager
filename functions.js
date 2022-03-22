3c5ca5106b3de41b57b6fcd844682c78bdca3663
Mariana Saraiva <marianascm8@gmail.com>
2022-03-22 18:47:16 -0300

    fix: update conditional name
    Co-authored-by: Tainá Santana <tainnaps@gmail.com>

--- functions.js
+++ functions.js
@@ -61,7 +61,7 @@ function validPassword(req, res, next) {
 function validName(req, res, next) {
   const { name } = req.body;
   if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
-  if (name.length < 4) {
+  if (name.length < 3) {
     return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
   next();
